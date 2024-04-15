b3e8fddf56dee7d93830df98f5932e2e3c5a9d6e
salesupper <saleslimeira@hotmail.com>
2024-04-15 16:23:22 -0300

    primeira parte projeto

---
+++ README.txt
@@ -0,0 +1,3 @@
+Python AI Backend Developer
+Este projeto foi constituído a partir do bootcamp da DIO em parceria com a Vivo 
+link do bootcamp = https://web.dio.me/track/coding-future-vivo-python-ai-backend-developer
---
+++ banco_v1.py
@@ -0,0 +1,60 @@
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+saques = 0
+LIMITE_SAQUES = 3
+
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        deposito = float(input("digite o valor do depósito R$"))
+        
+        if deposito <= 0:
+            print("deposito invalido tente novamente")
+        else:
+            extrato += f"Depósito: R$ {deposito:.2f}\n"
+            saldo += deposito
+
+    elif opcao == "s":
+        if saques >= LIMITE_SAQUES:
+            print("limite de saques atingido")
+        
+        else:
+            valor = float(input("digite o valor do saque R$"))
+        
+            if valor > saldo:
+                print("saldo insuficiente para saque")
+            elif valor > limite:
+                print("saque disponível apenas para valores até R$500,00")
+            else:
+                saldo -= valor
+                extrato += f"Saque: R$ {valor:.2f}\n"
+                saques += 1
+
+    elif opcao == "e":
+        print("\n================ EXTRATO ================")
+        if not extrato:
+            print("Não foram realizadas movimentações.")
+        else:
+            print(extrato)
+            print(f"\nSaldo: R$ {saldo:.2f}")  
+        print("==========================================")
+       
+    elif opcao == "q":
+        print("\noperação encerrada")
+        break
+    
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
